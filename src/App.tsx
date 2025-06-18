55cc7056c16cd3a09c317053b9968c228fda2c6a
cristianbgp <cristian.granda.pastor@gmail.com>
2025-06-18 17:40:20 -0500

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import Spinner from "./components/Spinner";
+import { Skeleton } from "./components/ui/skeleton";
 
 type Holiday = {
   name: string;
@@ -33,32 +35,39 @@ function App() {
   });
 
   return (
-    <div className="h-screen flex-col w-full bg-gray-200 gap-10 flex items-center justify-center">
-      {isHolidayLoading ? (
-        <p>Loading...</p>
-      ) : (
+    <div className="min-h-svh p-10 flex-col w-full flex items-center justify-center">
+      <div className="flex flex-col shrink-0 grow-0 flex-[2] justify-center items-center gap-2 w-full p-4">
         <div className="flex flex-col justify-center items-center gap-2">
           <h1 className="text-4xl font-bold">Is it 🇵🇪 holiday?</h1>
-          <p className="font-bold text-2xl">
-            {isHolidayData?.isHoliday ? "Yes ✅" : "No ❌"}
-          </p>
+          {isHolidayLoading ? (
+            <Spinner />
+          ) : (
+            <p className="font-bold text-2xl">
+              {isHolidayData?.isHoliday ? "Yes ✅" : "No ❌"}
+            </p>
+          )}
         </div>
-      )}
-      {holidaysLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="flex flex-col gap-2 bg-gray-400 p-4 rounded-2xl">
-          <p className="font-bold text-2xl">Next holidays</p>
-          <div className="flex flex-col gap-2">
-            {holidaysData?.map((holiday) => (
-              <p key={holiday.date} className=" text-xl">
-                <span className="font-bold">{formatDate(holiday.date)}</span>{" "}
-                {holiday.name}
-              </p>
-            ))}
+      </div>
+      <div className="flex flex-col flex-1 justify-center items-center gap-2 w-full p-4">
+        {holidaysLoading ? (
+          <Skeleton className="w-[400px] h-[400px]" />
+        ) : (
+          <div className="flex flex-col gap-2 p-4 rounded-2xl">
+            <p className="font-bold text-2xl">Next holidays</p>
+            <div className="flex flex-col gap-2">
+              {holidaysData?.map((holiday) => (
+                <div
+                  key={holiday.date}
+                  className="flex items-center gap-4"
+                >
+                  <p className="font-bold min-w-[100px]">{formatDate(holiday.date)}</p>
+                  <p className="text-xl">{holiday.name}</p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
